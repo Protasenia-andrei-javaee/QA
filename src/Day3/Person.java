b15277f3889d389c3e0acbfda7b5e3352c6ea57e
Andrei Protasenia <protasenia.javaee@gmail.com>
2020-04-05 09:09:29 +0300

    Added class Person

---
+++ src/Day3/Person.java
@@ -0,0 +1,19 @@
+package Day3;
+
+public class Person extends Human {
+    private String name;
+    private String surName;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setSurName(String surName) {
+        this.surName = surName;
+    }
+    public String getName() {
+        return name;
+    }
+    public String getSurName() {
+        return surName;
+    }
+}
